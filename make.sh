cad670b4c1aa0f786d1df31811592a694ba0e28f
Xiangdong Zeng <xdzeng96@gmail.com>
2020-03-04 03:03:18 +0800

    fix typo in make.sh

--- make.sh
+++ make.sh
@@ -3,13 +3,13 @@
 JOBNAME=latex-talk
 MAIN=latex-talk.tex
 
-if [ $1 == "-c" ]
+if [ "$1" == "-c" ]
 then
     latexmk -c -f $JOBNAME
     latexmk -c -f $JOBNAME-handout
     rm -f *.xdv *.synctex.gz
 else
-    if [ $1 == "-h" ]
+    if [ "$1" == "-h" ]
     then
         JOBNAME=$JOBNAME-handout
         echo "\\def\\HANDOUT{}" >> $JOBNAME.make.tex
